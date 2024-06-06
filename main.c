9aabce54a7a6219b7e4c2de762cbeff3701f4003
00chrism <christianmayamba04@gmail.com>
2024-06-06 11:29:15 +0200

    Ajout du choix de l'utilisateur (version 1.2)

--- main.c
+++ main.c
@@ -2,9 +2,63 @@
 #include <stdlib.h>
 
 
+// Affichage de l'écran d'acceuil
+void ecran_accueil()
+{
+    printf("\t1. Demarrer\n");
+    printf("\t2. Configurer\n");
+    printf("\t3. Aide\n");
+    printf("\t4. Quitter\n");
+}
 
+void demarrer()
+{
+
+}
+
+void configurer()
+{
+
+}
+
+void aide()
+{
+
+}
+
+void quitter()
+{
+
+}
+
+int choix_utilisateur(int choix)
+{
+    switch (choix)
+    {
+    case 1:
+        demarrer();
+        break;
+    case 2:
+        configurer();
+        break;
+    case 3:
+        aide();
+        break;
+    case 4:
+        quitter();
+        break;
+    default:
+        printf("Choix invalide.\n");
+        break;
+    }
+}
 int main()
 {
-    printf("Hello world!\n");
+    int choix;
+    ecran_accueil();
+
+    printf("Entrez votre choix: \n ==> ");
+    scanf("%d", &choix);
+    choix_utilisateur(choix);
     return 0;
 }
